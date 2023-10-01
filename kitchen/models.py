1f9b61e74cc4390f24af8c9ba95b5043192503b3
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-01 23:26:23 +0200

    Add models, update settings.py, update .gitignore
    Add models of Cook, Dish and DihType, add AUTH_USER_MODEL to settings.py, update .gitignore

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,42 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ("name", )
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ("username", )
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="dishes"
+    )
+
+    class Meta:
+        ordering = ("name", )
+        verbose_name_plural = "dishes"
+
+    def __str__(self) -> str:
+        return f"{self.name}: price - {self.price} ({self.dish_type.name})"
