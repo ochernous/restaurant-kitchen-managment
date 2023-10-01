03bc2eb32a35bfbc1a3521b13ae6cb3630415dec
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-01 23:47:27 +0200

    Add simple kitchen home page
    Update 'index' view, add 'index' template to kitchen dir in templates dir

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from kitchen.models import Cook, Dish, DishType
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Welcome to Kitchen Home page</h1>")
+    context = {
+        "num_cooks": Cook.objects.count(),
+        "num_dishes": Dish.objects.count(),
+        "num_dishtypes": DishType.objects.count()
+    }
+    return render(request, "kitchen/index.html", context=context)
