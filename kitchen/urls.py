8f8f9a3884da82c73f403297e020f5386d01cf5f
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-02 07:29:22 +0200

    Add web-page with DishTypes
    Add 'dish-types/' path to urlpatterns, add DishTypeListView to view.py, add dish_type_list.html template

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from kitchen.views import index
+from kitchen.views import index, DishTypeListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Cook, Dish, DishType
 
@@ -11,3 +12,9 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_dishtypes": DishType.objects.count()
     }
     return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type_list.html"
+    context_object_name = "dish_type_list"
