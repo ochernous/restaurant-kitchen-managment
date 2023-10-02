5728565a0a77fb2bf1084591c605b6c835b33ea5
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-02 19:53:17 +0200

    fixed 'DishType' model

--- kitchen/models.py
+++ kitchen/models.py
@@ -6,11 +6,11 @@ from django.db import models
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
-    def __str__(self) -> str:
-        return self.name
-
     class Meta:
         ordering = ("name", )
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Cook(AbstractUser):
