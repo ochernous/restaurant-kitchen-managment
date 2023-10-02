4bf0e3588cc25cbd2a7798934ae9411793bcfb5d
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-02 18:00:00 +0200

    Add feature - delete Dish and DishType
    add pathes for update Dish and DishType, add views for update Dish and DishType, add delete confirmation templates

--- kitchen/views.py
+++ kitchen/views.py
@@ -41,6 +41,12 @@ class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = "kitchen/dish_type_form.html"
 
 
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("kitchen:dish-type-list")
+    template_name = "kitchen/dish_type_confirm_delete.html"
+
+
 class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     queryset = Dish.objects.select_related("dish_type")
@@ -61,6 +67,11 @@ class DishCreateView(LoginRequiredMixin, generic.CreateView):
 class DishUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Dish
     fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Dish
     success_url = reverse_lazy("kitchen:dish-list")
 
 
