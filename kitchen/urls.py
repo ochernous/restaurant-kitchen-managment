1ebbdd0587ad04a7f7ebf962e9422ef760ebec70
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-02 08:02:19 +0200

    Add web-page with Cooks
    Add 'cooks/' path to urlpatterns, add 'CookListView' to views.py, add 'cook_list.html' template, fix typos in 'dish_list.html' and 'dish_type_list.html'

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from kitchen.views import index, DishTypeListView, DishListView
+from kitchen.views import (
+    index,
+    DishTypeListView,
+    DishListView,
+    CookListView
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -22,3 +22,7 @@ class DishTypeListView(generic.ListView):
 
 class DishListView(generic.ListView):
     model = Dish
+
+
+class CookListView(generic.ListView):
+    model = Cook
