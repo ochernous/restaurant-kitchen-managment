68d6f5489856b67dfd47aa034a895bbc4d3051e3
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-02 08:15:22 +0200

    Add django-debug-toolbar
    Add django-debug-toolbar, update settings.py and ursl.py

--- restaurant_kitchen/urls.py
+++ restaurant_kitchen/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("kitchen.urls", namespace="kitchen")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
