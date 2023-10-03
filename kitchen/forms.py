c86420d3f57e2a38b358e1cadaffe03ef74b729f
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-03 13:26:30 +0200

    Fix CookUpdateForm

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
 
 from kitchen.models import Cook, Dish
 
@@ -14,11 +14,29 @@ class CookCreationForm(UserCreationForm):
 
 
 class CookUpdateForm(UserChangeForm):
+    password = ReadOnlyPasswordHashField(
+        label="Password",
+        help_text=(
+            "Raw passwords are not stored, so there is no way to see this "
+            "user’s password"
+        ),
+    )
+
     class Meta:
         model = Cook
         fields = (
             "email", "years_of_experience"
         )
+
+    def save(self, commit=True):
+        cook = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        years_of_experience = self.cleaned_data.get("years_of_experience")
+        cook.email = email
+        cook.years_of_experience = years_of_experience
+        if commit:
+            cook.save()
+        return cook
 
 
 class DishForm(forms.ModelForm):
