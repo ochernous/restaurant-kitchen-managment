90a51a368af12aef70c176524dd419b6c5d2f4c9
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-08 22:41:15 +0200

    Update 'settings.py' - add 'restaurant-kitchen-mate.onrender.com' to ALLOWED_HOSTS

--- restaurant_kitchen/settings.py
+++ restaurant_kitchen/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+# DEBUG = "RENDER" not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-mate.onrender.com"]
 
 
 # Application definition
