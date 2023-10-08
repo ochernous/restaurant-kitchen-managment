dcfcb1e7ac9b9659f23c60debe56df6ec5caa747
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-08 17:07:20 +0200

    Update 'test_forms.py'

--- kitchen/tests/test_forms.py
+++ kitchen/tests/test_forms.py
@@ -4,7 +4,9 @@ from kitchen.forms import CookCreationForm
 
 
 class FormsTests(TestCase):
-    def test_cook_creation_form_with_first_last_name_email_experience_is_valid(self):
+    def test_cook_creation_form_with_first_last_name_email_experience_is_valid(
+        self,
+    ):
         form_data = {
             "username": "new_user",
             "password1": "12pass34word",
@@ -12,7 +14,7 @@ class FormsTests(TestCase):
             "first_name": "Name",
             "last_name": "Surname",
             "email": "user@mail.com",
-            "years_of_experience": 3
+            "years_of_experience": 3,
         }
         form = CookCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
