33d7eebb4ba5827c896b658981c3a61b86a0f572
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-08 16:53:27 +0200

    Add 'test_forms.py'

---
+++ kitchen/tests/test_forms.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from kitchen.forms import CookCreationForm
+
+
+class FormsTests(TestCase):
+    def test_cook_creation_form_with_first_last_name_email_experience_is_valid(self):
+        form_data = {
+            "username": "new_user",
+            "password1": "12pass34word",
+            "password2": "12pass34word",
+            "first_name": "Name",
+            "last_name": "Surname",
+            "email": "user@mail.com",
+            "years_of_experience": 3
+        }
+        form = CookCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEquals(form.cleaned_data, form_data)
