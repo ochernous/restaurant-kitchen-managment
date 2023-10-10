9e4dc51e756d06ca79c7ea671f4773d334dcd6fd
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-10 15:37:26 +0200

    Fix 'settings.py'

--- restaurant_kitchen/settings.py
+++ restaurant_kitchen/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-mate.onrender.com"]
 
