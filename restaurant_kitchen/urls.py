bb45108b8b2b1c1ec8b3ebdac616056561bc675e
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-10 15:11:31 +0200

    Add static files

--- restaurant_kitchen/urls.py
+++ restaurant_kitchen/urls.py
@@ -19,9 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = ([
     path("admin/", admin.site.urls),
     path("", include("kitchen.urls", namespace="kitchen")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+])
+               # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
