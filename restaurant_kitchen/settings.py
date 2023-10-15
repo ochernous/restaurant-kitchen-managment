c8e3f57677bf405034510e62d268c77416776123
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-15 15:34:23 +0200

    Fix 'settings.py' - delete redundant comment

--- restaurant_kitchen/settings.py
+++ restaurant_kitchen/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
-# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "restaurant-kitchen-mate.onrender.com"]
 
